4bfe5342cf14f089acc5f643ee71d5845eac005c
123roxy00 <123roxy00@gmail.com>
2021-02-13 18:45:30 -0500

    Updated for Week 4 Assignment.

--- Game Engines 4/Game/Scenes/GameScene.cpp
+++ Game Engines 4/Game/Scenes/GameScene.cpp
@@ -2,17 +2,40 @@
 
 GameScene::GameScene()
 {
-
+	shape = nullptr;
 }
 
 GameScene::~GameScene()
 {
-
+	delete shape;
+	shape = nullptr;
 }
 
 bool GameScene::OnCreate()
 {
 	Debug::Info("Game scene", __FILE__, __LINE__);
+
+	Vertex v;
+	std::vector<Vertex> vertexList;
+	vertexList.reserve(3);
+	v.position = glm::vec3(-0.5f, 0.5f, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(-0.5f, -0.5f, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(0.5f, -0.5f, 0.0f);
+	vertexList.push_back(v);
+
+	v.position = glm::vec3(0.5f, 0.5f, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(-0.5f, 0.5f, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(0.5f, -0.5f, 0.0f);
+	vertexList.push_back(v);
+
+	Model* model = new Model();
+	model->AddMesh(new Mesh(vertexList));
+	shape = new GameObject(model);
+
 	return true;
 }
 
@@ -23,5 +46,6 @@ void GameScene::Update(const float deltaTime_)
 
 void GameScene::Render()
 {
-
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	shape->Render();
 }
